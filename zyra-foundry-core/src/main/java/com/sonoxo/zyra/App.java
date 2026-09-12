099c189cb03552d05c0f7f7bc2ed9d27de7f6820
Douglas M Brown <zyra.localhost@gmail.com>
2026-09-11 20:30:10 -0400

    feat: activate Zyra Foundry runtime

--- zyra-foundry-core/src/main/java/com/sonoxo/zyra/App.java
+++ zyra-foundry-core/src/main/java/com/sonoxo/zyra/App.java
@@ -1,10 +1,16 @@
 package com.sonoxo.zyra;
 
-/**
- * Hello world!
- */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("======================================");
+        System.out.println(" ZYRA FOUNDRY CORE");
+        System.out.println("======================================");
+        System.out.println("STATUS  : ONLINE");
+        System.out.println("JAVA    : " + System.getProperty("java.version"));
+        System.out.println("MAVEN   : BUILD SUCCESS");
+        System.out.println("VERSION : 0.1.0");
+        System.out.println("TARGET  : PALANTIR FOUNDRY");
+        System.out.println("NEXT    : FOUNDRY API CONNECTION");
+        System.out.println("======================================");
     }
 }
